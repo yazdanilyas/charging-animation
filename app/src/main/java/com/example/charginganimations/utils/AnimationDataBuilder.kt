86dda59864fc99c003b0f7107926a4769faab023
Yazdan <yazdanilyas@gmail.com>
2022-07-13 02:40:24 +0500

    added more heart and circle animations

--- app/src/main/java/com/example/charginganimations/utils/AnimationDataBuilder.kt
+++ app/src/main/java/com/example/charginganimations/utils/AnimationDataBuilder.kt
@@ -21,16 +21,29 @@ object AnimationDataBuilder {
 
     fun heartAnimationsBuilder(): ArrayList<Int> {
         val heartAnimations = ArrayList<Int>()
-        heartAnimations.add(R.raw.animation_heart_1)
-        heartAnimations.add(R.raw.animation_heart_2)
-        heartAnimations.add(R.raw.animation_heart_3)
+        heartAnimations.add(R.raw.heart_1)
+        heartAnimations.add(R.raw.heart_2)
+        heartAnimations.add(R.raw.heart_3)
+        heartAnimations.add(R.raw.heart_4)
+        heartAnimations.add(R.raw.heart_5)
+        heartAnimations.add(R.raw.heart_6)
+        heartAnimations.add(R.raw.heart_7)
+        heartAnimations.add(R.raw.heart_8)
+        heartAnimations.add(R.raw.heart_9)
         return heartAnimations;
     }
 
     fun circleAnimationsBuilder(): ArrayList<Int> {
         val circleAnimations = ArrayList<Int>()
-        circleAnimations.add(R.raw.circle_animation_1)
-        circleAnimations.add(R.raw.circle_animation_2)
+        circleAnimations.add(R.raw.circle_1)
+        circleAnimations.add(R.raw.circle_2)
+        circleAnimations.add(R.raw.circle_3)
+        circleAnimations.add(R.raw.circle_4)
+        circleAnimations.add(R.raw.circle_5)
+        circleAnimations.add(R.raw.circle_6)
+        circleAnimations.add(R.raw.circle_7)
+        circleAnimations.add(R.raw.circle_8)
+        circleAnimations.add(R.raw.circle_9)
         return circleAnimations;
     }
 
