4fd06b66ccea99744665189853490567f435f2c5
Yazdan <yazdanilyas@gmail.com>
2022-08-31 01:49:33 +0500

    working on lock screen animations

--- app/src/main/java/com/example/charginganimations/receivers/LockScreenReceiver.kt
+++ app/src/main/java/com/example/charginganimations/receivers/LockScreenReceiver.kt
@@ -3,9 +3,20 @@ package com.example.charginganimations.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
+import com.example.charginganimations.ui.activities.plugin.ChargingActivity
 
 class LockScreenReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
+        if (intent?.action.equals(Intent.ACTION_SCREEN_ON)) {
+//            val powerConnectionIntent = Intent(context, ChargingActivity::class.java)
+//            powerConnectionIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+//            context?.startActivity(powerConnectionIntent)
+        } else if (intent?.action.equals(Intent.ACTION_SCREEN_OFF)) {
+            val powerConnectionIntent = Intent(context, ChargingActivity::class.java)
+            powerConnectionIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            context?.startActivity(powerConnectionIntent)
+            Toast.makeText(context, "screen off", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/charginganimations/ui/activities/plugin/ChargingActivity.kt
+++ app/src/main/java/com/example/charginganimations/ui/activities/plugin/ChargingActivity.kt
@@ -1,17 +1,19 @@
 package com.example.charginganimations.ui.activities.plugin
 
+import android.app.KeyguardManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.charginganimations.databinding.ActivityChargingBinding
 import com.example.charginganimations.utils.AppUtils
 import com.example.charginganimations.utils.CommonKeys
 import com.example.charginganimations.utils.PrefUtils
+
 
 class ChargingActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityChargingBinding
@@ -26,19 +28,32 @@ class ChargingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+            val keyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager?.requestDismissKeyguard(this, null)
+        } else {
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+            )
+        }
+
         mBinding = ActivityChargingBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
         setStatusBarAndBackgroundColor()
-        val localBroadcastManager = LocalBroadcastManager.getInstance(this)
-        val filter = IntentFilter()
-        filter.addAction("ACTION_FINISH")
-        localBroadcastManager.registerReceiver(activityFinishReceiver, filter)
+//        val localBroadcastManager = LocalBroadcastManager.getInstance(this)
+//        val filter = IntentFilter()
+//        filter.addAction("ACTION_FINISH")
+//        localBroadcastManager.registerReceiver(activityFinishReceiver, filter)
         showAnimation()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(activityFinishReceiver)
+//        unregisterReceiver(activityFinishReceiver)
     }
 
     private fun showAnimation() {
